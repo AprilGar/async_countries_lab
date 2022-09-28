7eab405755817e8d006cca945597bc2aea0343ef
April Garlington <april.garlington@hotmail.co.uk>
2022-09-28 17:32:04 +0100

    started on extension

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ document.addEventListener("DOMContentLoaded", () => {
         const jsonData = await response.json();
         const countriesData = jsonData;
 
-        // make a map of each countries from the data array
+        
+        // make a map of each countries common name from the data array
+        // the common name is down 2 levels so have to map twice
+        // collect all name objects
         const countriesNames = countriesData.map((country) => country.name);
+        // collect common name from within name 
         const countriesCommon = countriesNames.map((countryName) => countryName.common); 
+        
         // want a list of items 
-        // will need to loop through all items and append to list
+        // will need to loop through all items and append common name to list
         // this will find the first list and as I only have 1 this if fine
         const countriesUl = document.querySelector("ul")
         countriesCommon.forEach((countryCommonName) => {
@@ -27,7 +32,12 @@ document.addEventListener("DOMContentLoaded", () => {
             countriesUl.appendChild(nameLi);
         });
 
-    }
+        //Extension - attach flag
+        const countriesFlag = countriesData.map((flag) => country.flag);
+        countriesFlag.forEach((countriesFlag) => {
+            countriesFlag.appendChild(nameLi);
+        });
+    };
     
     // only do this when button is clicked
     const button= document.querySelector("#countries-button");
